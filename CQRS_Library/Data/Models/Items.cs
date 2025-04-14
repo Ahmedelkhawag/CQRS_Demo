8f5e308e71ff8da33d290df9216a48f923bde9ad
Ahmed Elkhawaga <AhmedElkhawaga028@gmail.com>
2025-04-14 18:38:06 +0200

    Create the CQRS project Creating Application Context Applying Migration And Updating Data base

---
+++ CQRS_Library/Data/Models/Items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_Library.Data.Models
+{
+    public class Items
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+       // public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
